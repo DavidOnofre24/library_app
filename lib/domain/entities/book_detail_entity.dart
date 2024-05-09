892b88d508198470484106ea8afb23e3fd2864fb
David Onofre <davidonofre1010@gmail.com>
2024-05-08 21:51:16 -0500

    Add book entity and book detail entity classes

---
+++ lib/domain/entities/book_detail_entity.dart
@@ -0,0 +1,31 @@
+class BookDetailEntity {
+  final String title;
+  final String subtitle;
+  final String authors;
+  final String publisher;
+  final String isbn13;
+  final String pages;
+  final String year;
+  final String rating;
+  final String desc;
+  final String price;
+  final String image;
+  final String url;
+  final String pdf;
+
+  BookDetailEntity({
+    required this.title,
+    required this.subtitle,
+    required this.authors,
+    required this.publisher,
+    required this.isbn13,
+    required this.pages,
+    required this.year,
+    required this.rating,
+    required this.desc,
+    required this.price,
+    required this.image,
+    required this.url,
+    required this.pdf,
+  });
+}
---
+++ lib/domain/entities/book_entity.dart
@@ -0,0 +1,17 @@
+class BookEntity {
+  final String title;
+  final String subtitle;
+  final String isbn13;
+  final String price;
+  final String image;
+  final String url;
+
+  BookEntity({
+    required this.title,
+    required this.subtitle,
+    required this.isbn13,
+    required this.price,
+    required this.image,
+    required this.url,
+  });
+}
