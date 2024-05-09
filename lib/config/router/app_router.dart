c6e5a3e6428c9131322f2b3476e3b06536ad84ae
David Onofre <davidonofre1010@gmail.com>
2024-05-09 00:00:50 -0500

    update app_router.dart to use Flutter Bloc for state management and inject dependencies

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,13 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:library_app/config/dependency_injection/dependency_injection.dart';
 import 'package:library_app/presentation/screens/screens.dart';
+import 'package:library_app/presentation/providers/providers.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    ),
+        path: '/',
+        name: HomeScreen.name,
+        builder: (context, state) => BlocProvider(
+              create: (context) =>
+                  BookCubit(bookRepository: getIt.get())..getReleaseBooks(),
+              child: const HomeScreen(),
+            ),
+        routes: [
+          GoRoute(
+            path: 'book-detail/:id',
+            name: BookDetailScreen.name,
+            builder: (context, state) => BlocProvider(
+              create: (context) => BookDetailCubit(bookRepository: getIt.get())
+                ..getBookDetail(state.params['id'] ?? '0'),
+              child: const BookDetailScreen(),
+            ),
+          ),
+        ]),
   ],
 );
---
+++ lib/presentation/providers/providers.dart
@@ -0,0 +1,2 @@
+export './book/cubit/book_cubit.dart';
+export './book_detail/cubit/book_detail_cubit.dart';
