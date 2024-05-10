399c719faf92d59d55253a89a4bc8bcf674380a3
David Onofre <davidonofre1010@gmail.com>
2024-05-09 20:17:28 -0500

    Rename use case classes for consistency

--- lib/domain/use_cases/get_book_detail_use_case.dart
+++ lib/domain/use_cases/get_book_detail_use_case.dart
@@ -1,10 +1,10 @@
 import 'package:library_app/domain/entities/book_detail_entity.dart';
 import 'package:library_app/domain/repositories/book_repository.dart';
 
-class GetBookDetail {
+class GetBookDetailUseCase {
   final BookRepository _repository;
 
-  GetBookDetail(this._repository);
+  GetBookDetailUseCase(this._repository);
 
   Future<BookDetailEntity> execute(String isbn13) async {
     return await _repository.getBookDetail(isbn13);
--- lib/domain/use_cases/get_release_books_use_case.dart
+++ lib/domain/use_cases/get_release_books_use_case.dart
@@ -1,10 +1,10 @@
 import 'package:library_app/domain/entities/book_entity.dart';
 import 'package:library_app/domain/repositories/book_repository.dart';
 
-class GetReleaseBooks {
+class GetReleaseBooksUseCase {
   final BookRepository _bookRepository;
 
-  GetReleaseBooks(this._bookRepository);
+  GetReleaseBooksUseCase(this._bookRepository);
 
   Future<List<BookEntity>> execute() async {
     return await _bookRepository.getNewReleaseBooks();
